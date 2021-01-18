fe3979432365ad5ebcefc098c3172272239a1dc6
Antoine Rousseau <antoine@rousseau.im>
2021-01-18 21:14:33 +0100

    create new typescript react-app yarn create react-app planning --template typescript

--- src/Logo/Logo.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Image } from "@chakra-ui/core";
-import src from "./logo.jpg";
-
-const Logo = props => {
-  return <Image {...props} src={src} alt="Logo de La Chouette Coop" />;
-};
-
-export default Logo;
