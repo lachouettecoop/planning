6915c26c7866305f3347249ba5eba5d726539ab2
Antoine Rousseau <antoine@rousseau.im>
2021-02-23 13:46:24 +0100

    fix build

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,6 +1,8 @@
 export const idRoleGH = "/api/roles/1"
 export const idRoleCaissier = "/api/roles/3"
 
+// https://github.com/typescript-eslint/typescript-eslint/issues/325
+// eslint-disable-next-line no-shadow
 export enum statusPiaf {
   Remplacement = "remplacement",
   Disponible = "",
