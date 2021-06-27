61d2376425fb2e4560edb2bcacb1500e21d0881d
silvia <s.alvarez.carrera@hotmail.com>
2021-06-27 09:00:02 +0200

    BdM page added

--- src/pages/authenticated/BdM.tsx
+++ src/pages/authenticated/BdM.tsx
@@ -1,5 +1,14 @@
+import { Typography } from "@material-ui/core"
+
+import Users from "src/components/Users"
+
 const BdMPage = () => {
-  return <>BdM - en construction</>
+  return (
+    <>
+      <Typography variant="h2">Groupe BdM</Typography>
+      <Users />
+    </>
+  )
 }
 
 export default BdMPage
