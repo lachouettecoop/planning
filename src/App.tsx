36ddf91c1ac593f489411fa5ac84fb14288fa3bf
Antoine Rousseau <antoine@rousseau.im>
2021-07-15 17:31:04 +0200

    tmp test version warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { BrowserRouter } from "react-router-dom"
 
 import { useUser } from "src/providers/user"
@@ -8,6 +9,16 @@ import { DialogProvider } from "src/providers/dialog"
 
 const App = () => {
   const { auth } = useUser()
+
+  useEffect(() => {
+    const redirect = window.confirm(`ATTENTION ! Cet outil n'est pas encore le planning officiel de La Chouette Coop !
+Il s'agit pour l'instant d'une version de développement, avec des données de test.
+Cliquez sur OK pour être redirigé·e vers la page de l'espace membre sur laquelle se trouve le lien vers le planning actuel.`)
+
+    if (redirect) {
+      window.location.href = "https://espace-membres.lachouettecoop.fr/page/tafs"
+    }
+  }, [])
 
   return (
     <BrowserRouter>
